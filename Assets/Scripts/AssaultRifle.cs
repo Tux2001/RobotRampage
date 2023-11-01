615d401817fc6b2920d6d88231b654fa1e665cec
tux2001 <deanmartel2001@outlook.com>
2023-11-01 19:50:02 -0400

    Ch9-Added Multiple Gun Logic Scripts && 'Constants' Script

---
+++ Assets/Scripts/AssaultRifle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssaultRifle : Gun
+{
+    override protected void Update()
+    {
+        base.Update();
+
+        //automatic update
+        if(Input.GetMouseButton(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/Shotgun.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Gun
+{
+    protected override void Update()
+    {
+       base.Update();
+
+       //shotgun & pistol have semi-auto fire rate
+       if(Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+       {
+           lastFireTime = Time.time;
+           Fire();
+       }
+    }
+}
