e06eb3f2f71f98d4e3db62bf63b1c94fbc51a2ef
Dean <deanmartel2001@outlook.com>
2023-11-13 13:34:37 -0500

    Ch10-Added 'PickupSpawn.cs' & Pickup Prefabs to Scene

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pickups;
+
+    //Instantiates a ramdom pickup & sets its position to PickupSpawn
+    void spawnPickup()
+    {
+        //Instantiate a random pickup
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    //2
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+
+    //3
+    void Start()
+    {
+        spawnPickup();
+    }
+
+    //4
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+}
