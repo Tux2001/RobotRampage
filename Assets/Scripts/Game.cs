98ddc85881a25f6af3bf2475e85ce9bc20690211
Dean <deanmartel2001@outlook.com>
2023-11-13 14:08:22 -0500

    Ch10-Added 'Game.cs' Script && Robot Spawns to Scene

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField] RobotSpawn[] spawns;
+    public int enemiesLeft;
+
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+
+    //Goes through array and calls SpawnRobot()
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+}
