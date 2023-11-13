41f3536315ad031d87d9fadfd1c09b4fed5ce6ca
Dean <deanmartel2001@outlook.com>
2023-11-13 17:04:31 -0500

    Ch11-Added 'Menu' Scene W/ Buttons & Logo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    //1
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+    //2
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
